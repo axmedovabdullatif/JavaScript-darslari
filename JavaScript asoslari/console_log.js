0c3bba6bfe35c61ee6d464b6db5865c2b8dcc5c2
2025 <axmedovabdullatif>
2026-01-01 22:48:05 +0500

    1-dars: console.log(); Izoh(comments) va arifmetik amallar tushuntirildi

---
+++ JavaScript asoslari/console_log.js
@@ -0,0 +1,124 @@
+// Muallif: Abdullatif
+// <==============================================================>
+// 📘 JavaScript bilan tanishuv: 1-DARS:
+// 1️⃣ JavaScript nima?
+// JavaScript (JS) — bu:
+// brauzerda ishlaydigan
+// web sahifani jonlantiradigan
+// foydalanuvchi bilan muloqot qiladigan dasturlash tili.
+// 📌 Agar:
+// HTML — skelet bo‘lsa
+// CSS — dizayn bo‘lsa
+// 👉 JavaScript — miya 🧠
+
+
+// JavaScript qayerda ishlaydi?
+// Browser Chrome, Edge va Firefox
+// Eng oson yo'l - console
+// index.html ulangan faylni open live server ochilganda
+// keyin Right click → Inspect → Console
+
+
+// Birinchi JavaScript kod
+console.log("Salom JavaScript!");
+// Natija: Salom Javascript!
+// console.log() - bu ekranga chiqarish
+// "Salom JavaScript!" - bu matn (string)
+// matn " " va ' ' ichida yoziladi
+
+
+// console.log() - nima uchun kerak?
+// Dasturchilar console.log() ni:
+// kodni tekshirish
+// xatoni topish
+// qiymatni ko‘rish
+// uchun ishlatadi misol:
+console.log(5+3);
+// natija: 8 chiqadi
+// raqamlar uchun " " va ' ' ishlatilmaydi (int)
+
+
+// Izohlar (Comments)
+// Izoh - bu kod emas JavaScript izohlarni (comments) o'qimaydi
+// 1qatorli izoh uchun // shu 2ta backslash ishlatiladi
+// Bu mening 1chi izohim
+// Ko'p qatorli izoh uchun /* */ belgisi ishlatiladi misol:
+/* Bu yerda bir nechta
+   izoh yozilgan */
+// Izohlar asosan kodni tushunishga yordam beradi.
+
+
+// JavaScript ni HTML orqali ulash (asosiy tushuncha) ni index.html da ko'rishingiz mumkin
+
+
+// Eng muhim qoidalar ⚠️
+// JavaScript katta-kichik harflarga sezgir bo'ladi
+// Console.log() // bunday yozish xato❌
+// console.log() // bunisi to'g'ri xisoblanadi✅
+
+
+// Xar bir buyruq odatda ; bilan tugaydi bu qator tugaganini bildiradi
+// matnlar = str(string) shu belgilar " " va ' ' ichiga yoziladi
+// raqamlar = int(integer) uchun " " va ' ' shart emas
+
+
+// Misollar
+console.log("Mening ismim Abdullatif");
+console.log("Men JavaScript o'rganishni boshladim");
+console.log(2026);
+
+
+// JavaScript da xisob-kitob o'zi xisoblaydi
+console.log(10 + 5);
+console.log(10 - 2);
+
+
+// ✅ Asosiy arifmetik amallar — 4 ta
+// JavaScript’da (va matematikada) asosiylari shu 4 ta:
+// lekin yana keng tarqalgan 2ta amal bor
+// shular bilan umumiy 6ta bo'ladi ular
+console.log(5 + 3); // 8
+console.log(10 - 6); // 4
+console.log(4 * 5); // 20
+console.log(14 / 7); // 2
+// % - qoldiqni topish bunda xisoblaydi faqat qoldiq raqamni chiqaradi
+console.log(7 % 2); // 1(qoldiq)
+// ++ faqat +1 oshirish (increment)
+let x = 5 // let o'zgaruvchini keyingi darslardan to'liq tushunib olamiz
+x ++;
+console.log(x);
+// -- faqat -1 kamaytirish(decrement)
+x --
+console.log(x);
+
+
+// 📝 Vazifa 1
+// Mening ismim Abdullatif
+// Men JavaScript o‘rganayapman
+// Bugun 1-dars
+// Shunday yozib console.log(); ga chiqaring
+
+
+// 📝 Vazifa 2 (Challenge)
+// Ism: Abdullatif
+// Kasb: Dasturchi
+// Til: JavaScript
+// Yoshim: 20
+// console.log(); ga chiqarib ko'ring
+
+
+// 📝 Vazifa 3
+//      console.log(10 % 3);
+//      let x = 13
+//      x ++;
+//      console.log(x);
+//      x --;
+//      console.log(x);
+// Shunga o'xshash misollar ishlab ko'ring
+
+
+// =====================================
+// ✅ 1-DARS XULOSA
+// Bugun console.log(), comment,
+// va arifmetik amallar bilan tanishdik
+// =====================================
